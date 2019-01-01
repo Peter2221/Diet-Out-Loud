ce0fee8caea95d0ddebcbfabf328fdbc53c1a9f0
Aleksandra Lorenc <unattainableonehere@gmail.com>
2019-01-01 23:40:44 +0100

    first working search

--- ExcelHandler.py
+++ ExcelHandler.py
@@ -3,23 +3,52 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 
 class ExcelHandler():
 
-    kalorie = []
     produkty = []
+    kalorie = []
 
     def open_workbook(self, filename):
-        wb = load_workbook("products.xlsx")  # wybieramy plik
+        wb = load_workbook(filename, data_only=True)  # wybieramy plik
+        return wb
 
     #nie jestem pewna czy ta funkcja jest potrzebna ale napisałam :3
     def assign_sheets(self, wb):
         self.produkty = wb["produkty"]  # wybieramy arkusz
         self.kalorie = wb["kalorie"]
 
-    def find_product(self, searchedProduct):
-        for row in range(1, self.produkty.max_row + 1):
-            for col in range(1, self.max_column + 1):
-                myCell = self.produkty.cell(row, col)
-                if myCell.value == searchedProduct:
-                    print("found: " + str(xl_rowcol_to_cell(row - 1, col - 1)))
+    def find_product(self):
+
+        while True:
+            searched_product = input("podaj produkt: ") #to zastapimy mowa + morfeusz
+
+            for row in range(1, self.produkty.max_row + 1):
+                for col in range(1, self.produkty.max_column + 1):
+                    my_cell = self.produkty.cell(row, col)
+                    if my_cell.value == searched_product:
+                        found_cell = xl_rowcol_to_cell(row - 1, col - 1)
+                        return found_cell
+                        break
+
+    def get_calories(self, found_cell):
+        kcal = self.kalorie[found_cell]
+        return kcal
+
+    def what_you_ate(self):
+        found_cell = self.find_product()
+        product = self.produkty[found_cell]
+        kcal = self.get_calories(found_cell)
+        print("zjadłeś produkt: " + str(product.value) + " co daje: " + str(kcal.value) + "kcal")
+
+
+exl = ExcelHandler()
+wb = exl.open_workbook("products.xlsx")
+exl.assign_sheets(wb)
+exl.what_you_ate()
+
+
+
+
+
+
 
 
 
